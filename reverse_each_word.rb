0588bcc659d139cf5ecb07820ebda98a1959ff17
Bleak Chandler <bleakchandler@gmail.com>
2021-01-26 17:18:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,6 +7,12 @@ new_sentence.join(" ")
 end
 
 
+reverse_each_word sentence = ("carrot cucumber pepper")
+
+
+
+
+
 def reverse_each_word(sentence)
   split_sentence = sentence.split
   split_sentence.collect {|word|
